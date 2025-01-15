c7bd88a06f5be667df1cba498369172012aef754
nisha783 <hm6980097@gmail.com>
2025-01-14 21:35:49 -0800

    get cartitem count

--- resources/views/livewire/cart-wrapper.blade.php
+++ resources/views/livewire/cart-wrapper.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- Stop trying to control. --}}
-</div>
+<li>
+    <a href="{{ route('cart.index') }}" class="icon">
+        <i class="fa-light fa-bag-shopping"></i>
+        <span>{{ $getCartItemsCount }}</span>
+    </a>
+    <div class="content">
+        <span>Your cart,</span>
+        <h5 class="number">{{ Number::currency($getCartTotal) }}</h5>
+    </div>
+</li>
--- resources/views/livewire/product-wrapper.blade.php
+++ resources/views/livewire/product-wrapper.blade.php
@@ -1,3 +1,48 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="shop-item">
+    <div class="shop-thumb">
+        <div class="overlay"></div>
+        @if ($product->images->count())
+            <img src="{{ Storage::url($product->images->first()->image) }}" alt="shop">
+        @endif
+        @if ($product->sale_price)
+            <span class="sale">Sale</span>
+        @endif
+        <ul class="shop-list">
+            <li><a href="javascript:void(0)" wire:click="addToCart({{ $product->id }})"><i
+                        class="fa-regular fa-cart-shopping"></i></a></li>
+            <li><a href="javascript:void(0)" wire:click="addToWishlist({{ $product->id }})"><i class="fa-light fa-heart"></i></a></li>
+            <li><a href="#"><i class="fa-light fa-eye"></i></a></li>
+        </ul>
+    </div>
+    <div class="shop-content">
+        <div class="d-flex flex-wrap">
+            @forelse ($product->categories as $category)
+                <a href="{{ route('category.show', $category->slug) }}">
+                    <span class="category badge badge-secondary">{{ $category->name }}</span>
+                </a>
+            @empty
+                <span class="category">{{ 'Uncategorized' }}</span>
+            @endforelse
+        </div>
+        <h3 class="title"><a href="{{ route('product.show', ['product' => $product->slug]) }}">{{ $product->name }}</a>
+        </h3>
+        <div class="review-wrap">
+            <ul class="review">
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-solid fa-star"></i></li>
+            </ul>
+            <span>({{ $product->reviews()->count() }} Reviews)</span>
+        </div>
+        <span class="price">
+            @if ($product->sale_price)
+                <span class="offer">{{ Number::currency($product->price) }}</span>
+                {{ Number::currency($product->sale_price) }}
+            @else
+                {{ Number::currency($product->price) }}
+            @endif
+        </span>
+    </div>
 </div>
